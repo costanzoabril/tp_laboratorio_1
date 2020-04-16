4545854266bc549460ad948e74dede983beeddf6
costanzoabril <60653153+costanzoabril@users.noreply.github.com>
2020-04-16 19:18:22 -0300

    Add files via upload

---
+++ TrabajoPractico1/tpUno.h
@@ -0,0 +1,10 @@
+#ifndef TPUNO_H_INCLUDED
+#define TPUNO_H_INCLUDED
+
+int menu(int a, int b);
+int sumar(int a, int b);
+int restar(int a, int b);
+int multiplicar(int a, int b);
+float dividir(float a, float b);
+
+#endif // TPUNO_H_INCLUDED
