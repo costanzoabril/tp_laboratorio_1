1ab98c5c0e2d86941781ddd0a563fd9e74e3bb19
costanzoabril <60653153+costanzoabril@users.noreply.github.com>
2020-06-28 01:00:39 -0300

    Add files via upload

---
+++ TrabajoPractico2/validaciones.h
@@ -0,0 +1,26 @@
+#ifndef VALIDACIONES_H_INCLUDED
+#define VALIDACIONES_H_INCLUDED
+
+int utn_getCadena(char *pAux, int limit, int retry, char* msg, char* msgError);
+
+int getCadena(char* pAux, int limit);
+
+int isChar(char str[]);
+
+int utn_getFlotante(float* pFloat, int retry, char* msg, char*msgError, float min, float max);
+
+int getFloat(float*pAux);
+
+int isFloat(char* pAux);
+
+int utn_getEntero(int* pEntero, int retry, char* msg, char*msgError, int min, int max);
+
+int getInt(int* pAux);
+
+int isInt(char *pAux);
+
+int strLwr(char* str);
+
+int normalizeStr(char* str);
+
+#endif // VALIDACIONES_H_INCLUDED
